b5f5c6812e7ca47bf68d3ec957e03d99cb55a32e
Steve <chengquan.wang@ele.me>
2017-06-23 18:19:58 +0800

    initial

---
+++ Sign/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  SignModule
+//
+//  Created by Steve on 2017/6/23.
+//  Copyright © 2017年 Jack. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
+    }
+}
---
+++ SignModule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SignModule
+//
+//  Created by Steve on 2017/6/23.
+//  Copyright © 2017年 Jack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
